e235f0c7550cf1105d4e04770d5ed90d41bc4ab4
flo7716 <florian.andre.77@gmail.com>
2025-03-10 14:18:42 +0100

    ajout if git clone

--- setup.sh
+++ setup.sh
@@ -16,11 +16,14 @@ sudo apt-get install -y git curl wget unzip python3 python3-pip python3-venv
 
 # Installation des paquets pour admin réseau et systèmes
 sudo apt-get install -y net-tools iproute2
-sudo apt-get install -y crontab
+sudo apt-get install -y crontab man
 
 # Installation de l'editeur de texte nano
 sudo apt-get install -y nano
 
 
-# Récupération du dépôt git
-git clone https://github.com/flo7716/cours_scrypt_linux/
+# Récupération du dépôt git si inexistant
+if [ ! -d "cours_scrypt_linux" ]; then
+    git clone https://github.com/flo7716/cours_scrypt_linux/
+fi
+
