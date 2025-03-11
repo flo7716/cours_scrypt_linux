4da97ffba4e8c6363008ef167d5a85f37ec52320
flori <161379261+flo7716@users.noreply.github.com>
2025-03-11 13:34:12 +0100

    ajout case

--- parcours.sh
+++ parcours.sh
@@ -6,6 +6,19 @@ for file in $(ls $WORKDIR);do
 	echo "-------------"
 	echo $(basename $file)
 	cat $file
+	case  $file in
+		*.py)
+			echo "C'est un script python"
+			;;
+		*.sh)
+			echo "C'est un script shell"
+			;;
+		*.txt)
+			echo "C'est un fichier texte"
+			;;
+		*)
+			echo "Type de fichier inconnu"
+			;;
 	echo "-------------"
 done
 
