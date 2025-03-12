767af089cc342dd6faf033413244efc993cfe14c
flo7716 <florian.andre.77@gmail.com>
2025-03-12 12:12:25 +0100

    replaced venv by conda

--- setup.sh
+++ setup.sh
@@ -15,11 +15,20 @@ sudo apt-get upgrade -y
 
 
 # Installation des paquets nécessaires
-sudo apt-get install -y git curl wget unzip python3 python3-pip python3-venv
+sudo apt-get install -y git curl wget unzip python3 python3-pip
 
 # Installation des paquets pour admin réseau et systèmes
 sudo apt-get install -y net-tools iproute2
 sudo apt-get install -y cron man
+
+# Installation d'Anaconda
+curl -O https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
+bash ~/Anaconda3-2024.10-1-Linux-x86_64.sh
+
+# Création d'un environnement Anaconda
+conda create --name projet_fed python=3.8
+conda activate projet_fed
+
 
 # Installation de l'editeur de texte nano
 sudo apt-get install -y nano
