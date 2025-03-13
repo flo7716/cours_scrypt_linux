a3c4c898d7d13a0038ae3488d001366ae80dbb22
flo7716 <florian.andre.77@gmail.com>
2025-03-13 08:49:47 +0100

    séparation scripts cours linux et projet fed

--- setup.sh
+++ setup.sh
@@ -12,6 +12,17 @@ sudo apt-get install -y git curl wget unzip python3 python3-pip
 sudo apt-get install -y net-tools iproute2
 sudo apt-get install -y cron man
 
+# Installation de l'editeur de texte nano
+sudo apt-get install -y nano
+
+
+# Récupération du dépôt git si inexistant
+if ! find / -type d -name "cours_scrypt_linux" 2>/dev/null | grep -q .; then
+    git clone https://github.com/flo7716/cours_scrypt_linux/
+fi
+
+### EXEMPLE DE SCRIPT D'INSTALLATION A AJOUTER POUR CREER UN ENVIRONNEMENT PYTHON DESTINE AU MACHINE LEARNING ###
+
 # Installation d'Anaconda (environnement virtuel Python)
 curl -O https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
 bash ~/Anaconda3-2024.10-1-Linux-x86_64.sh
@@ -21,12 +32,5 @@ conda create --name env_scrypt python=3.8
 conda activate env_scrypt
 
 
-# Installation de l'editeur de texte nano
-sudo apt-get install -y nano
 
-
-# Récupération du dépôt git si inexistant
-if ! find / -type d -name "cours_scrypt_linux" 2>/dev/null | grep -q .; then
-    git clone https://github.com/flo7716/cours_scrypt_linux/
-fi
 
